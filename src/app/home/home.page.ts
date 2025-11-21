6c9c042ba08b067fa0123087661d00eb66f8ba18
sam-210279 <sg.castro.21@gmail.com>
2025-11-20 20:03:17 -0500

    ion-text y ion-grid

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: 
+  [IonHeader, IonToolbar, 
+    IonTitle, IonContent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
+
 })
+
 export class HomePage {
   constructor() {}
 
