95e1e5d4a2d85d804e266f40da2aaaf15bd357c6
sam-210279 <sg.castro.21@gmail.com>
2025-12-12 19:00:37 -0500

    dia 12 diciembre

--- src/app/paginas/principal/principal.page.ts
+++ src/app/paginas/principal/principal.page.ts
@@ -1,12 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, ModalController } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { HeaderGlobalComponent } from 'src/app/componentes/headerglobal/headerglobal.component';
 import { FooterGlobalComponent } from 'src/app/componentes/footer-global/footer-global.component';
 import { VentanaEmergenteComponent } from 'src/app/componentes/ventana-emergente/ventana-emergente.component';
-import { ModalController } from '@ionic/angular/standalone';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
@@ -15,39 +14,37 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
   standalone: true,
-  imports: [ HttpClientModule, IonContent, CommonModule, FormsModule, HeaderGlobalComponent, FooterGlobalComponent],
+  imports: [ HttpClientModule, IonContent, CommonModule, FormsModule, HeaderGlobalComponent, FooterGlobalComponent, VentanaEmergenteComponent], // Agregué VentanaEmergenteComponent
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PrincipalPage implements OnInit {
-  [x: string]: any;
-    
- productos: any[]=[];
-  productosfiltrados: any[]= [];
-  categorias: any[]=[];
-   
-   constructor(
+  productos: any[] = [];
+  productosfiltrados: any[] = [];
+  categorias: any[] = [];
+
+  constructor(
     private router: Router,
     private modalCtrl: ModalController,
     private http: HttpClient
   ) {}
-  
- ngOnInit() {
+
+  ngOnInit() {
     this.cargarProductos();
     this.cargarCategorias();
   }
 
-  cargarCategorias(){
+   cargarCategorias(){
      this.http.get('assets/BD/categorias.json')
      .subscribe((data: any) => {
       this.categorias =data;
      });
   }
 
-
-    filtrarPorSubcategoria(nombre:string ){
-      this.productosfiltrados 
-      = this.productos.filter(p =>p.subcategoria === nombre)
+  filtrarPorSubcategorias(nombre: string) {
+    this.productosfiltrados = this.productos.filter(p => p.subcategorias === nombre);
   }
+    mostrarTodos() {
+  this.productosfiltrados = [...this.productos]}
 
    cargarProductos(){
      this.http.get('assets/BD/productos.json')
@@ -57,37 +54,50 @@ export class PrincipalPage implements OnInit {
   }
 
 
- irktm(producto:any){
-    this.router.navigate(['ktm'], {queryParams:producto});
+  irktm(producto: any) {
+    this.router.navigate(['ktm'], { queryParams: producto });
   }
 
-  filtrar(event:any) {
-   const texto = (event.target.value || '').toLowerCase().trim();
+  filtrar(event: any) {
+    const texto = (event.target.value || '').toLowerCase().trim();
 
-   if (texto === '') {
-     this.productosfiltrados = [...this.productos ];
-     return;
-   } 
+    if (texto === '') {
+      this.productosfiltrados = [...this.productos];
+      return;
+    }
 
-   this.productosfiltrados = this.productos.filter( 
-      
-    p => p.titulo.toLowerCase().includes(texto) ||
-     p.descripcion.toLowerCase().includes(texto) ||
-     p.precio.toString().includes(texto) 
-  
+    this.productosfiltrados = this.productos.filter(
+      p => p.titulo.toLowerCase().includes(texto) ||
+           p.descripcion.toLowerCase().includes(texto) ||
+           p.precio.toString().includes(texto)
     );
-
   }
-
-  async verimagengrande(producto: any){
+  async verimagengrande(producto: any) {
     const modal = await this.modalCtrl.create({
-      component:VentanaEmergenteComponent,
-      componentProps: { Imagen: producto.imagen, titulo: producto.titulo },
+      component: VentanaEmergenteComponent,
+      componentProps: { 
+        Imagen: producto.imagen, 
+        titulo: producto.titulo },
       cssClass: 'estilo-modal'
-   });
-   await modal.present();
-
-
+    });
+    await modal.present();
   }
+  imagenesCarrusel = [
+  'assets/img/duke 690 rs -004.jpg',
+  'assets/img/ktm_Stoked__87867.jpg',
+  'assets/img/ktm-1290-super-adventure-r-2021-1600-01.jpg',
+  'assets/img/ktm-adventure-.jpg'
+  
+];
 
+slideOpts = {
+  initialSlide: 0,
+  speed: 600,
+  autoplay: {
+    delay: 300
+  },
+  loop: true
+};
+ 
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { register } from 'swiper/element/bundle';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
@@ -12,3 +12,5 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
+register();
